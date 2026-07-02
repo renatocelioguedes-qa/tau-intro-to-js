ca9f4a2aa7fa7401c73689b842eb421bb098f05c
renatocelioguedes-qa <renatocelioguedes@gmail.com>
2026-07-01 23:13:14 -0300

    Create 06-if-else.js

---
+++ 06-if-else.js
@@ -0,0 +1,9 @@
+let attendance = 16;
+
+if (attendance < 15) {
+    console.log("Ainda temos vagas");
+} else if (attendance > 15) {
+    console.log("Estamos lotados");
+} else { 
+    console.log("Estamos na capacidade máxima");
+}
