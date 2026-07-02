784d88ef8ebd08252058f7f64509590093c0149b
renatocelioguedes-qa <renatocelioguedes@gmail.com>
2026-07-01 23:13:19 -0300

    Create 07-loops.js

---
+++ 07-loops.js
@@ -0,0 +1,11 @@
+const nomes = ["Renato", "João", "Maria", "José", "Ana"];
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i]);
+}
+
+const numeros = [1, 2, 3, 4, 5];
+
+for(let j = 0; j < numeros.length; j++) {
+    console.log(numeros[j] * 3);
+}
